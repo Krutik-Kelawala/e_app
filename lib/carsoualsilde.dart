ba6139eba33a30a96605d8f2296557c87efbe9f9
Krutik <krutikkelawala181818@gmail.com>
2022-05-14 00:34:31 +0530

    Api done

--- lib/carsoualsilde.dart
+++ lib/carsoualsilde.dart
@@ -1,4 +1,6 @@
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:e_app/main.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class pageview extends StatefulWidget {
@@ -11,31 +13,36 @@ class _pageviewState extends State<pageview> {
   int position = 0;
 
   List<String> img = [
-    "backgroundimg/p1.jpg",
-    "backgroundimg/p1.jpg",
-    "backgroundimg/p1.jpg",
-    "backgroundimg/p1.jpg"
+    "carouselimage/c1.png",
+    "carouselimage/c2.png",
+    "carouselimage/c3.png",
+    "carouselimage/c4.png",
+    "carouselimage/c5.png",
+    "carouselimage/c6.png",
+    "carouselimage/c7.png",
+    "carouselimage/c8.png",
+    "carouselimage/c9.png",
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFF84E586),
       body: SafeArea(
-        child: Column(
+        child: Stack(
           children: [
             CarouselSlider.builder(
-              itemCount: 5,
+              itemCount: img.length,
               itemBuilder: (context, index, realIndex) {
                 return Container(
                     width: MediaQuery.of(context).size.width,
-                    margin: EdgeInsets.symmetric(horizontal: 5.0),
-                    decoration: BoxDecoration(color: Colors.amber),
-                    child: Center(
-                      child: Center(
-                        child: Image.asset(
-                          "backgroundimg/pngwing.com.png",
-                          fit: BoxFit.fill,
-                        ),
+                    // margin: EdgeInsets.symmetric(horizontal: 1),
+                    decoration: BoxDecoration(color: Colors.greenAccent),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Image.asset(
+                        "${img[index]}",
+                        fit: BoxFit.cover,
                       ),
                     ));
               },
@@ -46,24 +53,51 @@ class _pageviewState extends State<pageview> {
                     position = index;
                   });
                 },
-                height: 500,
+                height: MediaQuery.of(context).size.height,
                 autoPlay: true,
                 enlargeCenterPage: true,
               ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: img.map((e) {
-                int index = img.indexOf(e);
-                return Container(
-                  height: 8,
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: position == index ? Colors.black : Colors.grey),
-                  width: 8,
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 2),
-                );
-              }).toList(),
+
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.center,
+            //   children: img.map((e) {
+            //     int index = img.indexOf(e);
+            //     return Align(
+            //       alignment: Alignment.bottomCenter,
+            //       child: Container(
+            //         height: 8,
+            //         decoration: BoxDecoration(
+            //             border: Border.all(width: 5),
+            //             shape: BoxShape.circle,
+            //             color: position == index ? Colors.black : Colors.grey),
+            //         width: 8,
+            //         margin: EdgeInsets.symmetric(vertical: 10, horizontal: 2),
+            //       ),
+            //     );
+            //   }).toList(),
+            // ),
+
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: OutlinedButton(
+                  onPressed: () {
+                    Navigator.pushReplacement(context, MaterialPageRoute(
+                      builder: (context) {
+                        return loginpage();
+                      },
+                    ));
+                  },
+                  style: OutlinedButton.styleFrom(
+                      elevation: 10,
+                      shadowColor: Colors.black,
+                      backgroundColor: Colors.lightBlueAccent,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                  child: Text(
+                    "Get Started",
+                    style: TextStyle(color: CupertinoColors.white),
+                  )),
             )
           ],
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:e_app/carsoualsilde.dart';
 import 'package:e_app/signuppage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: loginpage(),
+    home: pageview(),
   ));
 }
 
@@ -24,182 +26,187 @@ class _loginpageState extends State<loginpage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF84E586),
-      body: SafeArea(
-          child: SingleChildScrollView(
-              child: Container(
-                  // height: MediaQuery.of(context).size.height,
-                  // decoration: BoxDecoration(
-                  //     image: DecorationImage(
-                  //         opacity: 100,
-                  //         image: AssetImage("backgroundimg/p1.jpg"),
-                  //         fit: BoxFit.cover)),
-                  child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Container(
-            child: Image.asset("backgroundimg/pngwing.com.png"),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-            child: RichText(
-                text: TextSpan(
-              children: [
-                TextSpan(
-                    text: "Login ",
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 50,
-                        fontWeight: FontWeight.bold)),
-                TextSpan(
-                    text: "here",
-                    style: TextStyle(color: Colors.black, fontSize: 30))
-              ],
-            )),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  padding: EdgeInsets.all(5),
-                  child: TextField(
-                    onChanged: (value) {
-                      setState(() {
-                        usernamestatus = false;
-                      });
-                    },
-                    toolbarOptions: ToolbarOptions(
-                        selectAll: true, paste: true, cut: true, copy: true),
-                    controller: username,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10)),
-                        labelText: "User Name",
-                        hintText: "Enter Name here",
-                        prefixIcon: Icon(Icons.person),
-                        errorText:
-                            usernamestatus ? "Please enter your name" : null),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.all(5),
-                  child: TextField(
-                    onChanged: (value) {
-                      setState(() {
-                        passwordstatus = false;
-                      });
-                    },
-                    toolbarOptions: ToolbarOptions(
-                        selectAll: true, paste: true, cut: true, copy: true),
-                    obscureText: passhideshow,
-                    controller: password,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10)),
-                        labelText: "Password",
-                        hintText: "Enter Password here",
-                        prefixIcon: Icon(Icons.password),
-                        suffixIcon: IconButton(
-                            onPressed: () {
-                              setState(() {
-                                passhideshow = !passhideshow;
-                              });
-                            },
-                            icon: Icon(passhideshow
-                                ? Icons.visibility_off
-                                : Icons.visibility)),
-                        errorText: passwordstatus
-                            ? "Please enter your password"
-                            : null),
-                  ),
-                ),
-                Container(
-                  child: InkWell(
-                    onTap: () {},
-                    child: Text(
-                      "Forgot password?",
+      body: FadeInDown(
+        delay: Duration(seconds: 3),
+        duration: Duration(seconds: 5),
+        child: SafeArea(
+            child: SingleChildScrollView(
+                child: Container(
+                    // height: MediaQuery.of(context).size.height,
+                    // decoration: BoxDecoration(
+                    //     image: DecorationImage(
+                    //         opacity: 100,
+                    //         image: AssetImage("backgroundimg/p1.jpg"),
+                    //         fit: BoxFit.cover)),
+                    child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              child: Image.asset("backgroundimg/p2.png"),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              child: RichText(
+                  text: TextSpan(
+                children: [
+                  TextSpan(
+                      text: "Login ",
                       style: TextStyle(
-                          decoration: TextDecoration.underline,
-                          color: Colors.blue),
+                          color: Colors.black,
+                          fontSize: 50,
+                          fontWeight: FontWeight.bold)),
+                  TextSpan(
+                      text: "here",
+                      style: TextStyle(color: Colors.black, fontSize: 30))
+                ],
+              )),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(5),
+                    child: TextField(
+                      onChanged: (value) {
+                        setState(() {
+                          usernamestatus = false;
+                        });
+                      },
+                      toolbarOptions: ToolbarOptions(
+                          selectAll: true, paste: true, cut: true, copy: true),
+                      controller: username,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                          labelText: "User Name",
+                          hintText: "Enter Name here",
+                          prefixIcon: Icon(Icons.person),
+                          errorText:
+                              usernamestatus ? "Please enter your name" : null),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  child: ElevatedButton(
-                      onPressed: () {
-                        String pusername = username.text;
-                        String puserpassword = password.text;
-
+                  Container(
+                    padding: EdgeInsets.all(5),
+                    child: TextField(
+                      onChanged: (value) {
                         setState(() {
-                          if (pusername.isEmpty) {
-                            setState(() {
-                              usernamestatus = true;
-                            });
-                          }
-                          if (puserpassword.isEmpty) {
-                            setState(() {
-                              passwordstatus = true;
-                            });
-                          }
-                          // else {
-                          //   setState(() {
-                          //     usernamestatus = false;
-                          //     passwordstatus = false;
-                          //   });
-                          // }
+                          passwordstatus = false;
                         });
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          content: Text("LogIn Successsfully !"),
-                          duration: Duration(seconds: 2),
-                        ));
                       },
-                      style: ElevatedButton.styleFrom(
-                          elevation: 10,
-                          shadowColor: Colors.black,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10))),
-                      child: Text("Sign in")),
-                ),
-                InkWell(
-                  onTap: () {
-                    Navigator.pushReplacement(context, MaterialPageRoute(
-                      builder: (context) {
-                        return signuppg();
-                      },
-                    ));
-                  },
-                  child: Container(
-                    child: RichText(
-                        text: TextSpan(children: [
-                      TextSpan(
-                          text: "Don't have account ? ",
-                          style: TextStyle(fontSize: 10, color: Colors.black)),
-                      TextSpan(
-                          text: "Sign Up",
-                          style: TextStyle(fontSize: 20, color: Colors.blue))
-                    ])),
+                      toolbarOptions: ToolbarOptions(
+                          selectAll: true, paste: true, cut: true, copy: true),
+                      obscureText: passhideshow,
+                      controller: password,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                          labelText: "Password",
+                          hintText: "Enter Password here",
+                          prefixIcon: Icon(Icons.password),
+                          suffixIcon: IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  passhideshow = !passhideshow;
+                                });
+                              },
+                              icon: Icon(passhideshow
+                                  ? Icons.visibility_off
+                                  : Icons.visibility)),
+                          errorText: passwordstatus
+                              ? "Please enter your password"
+                              : null),
+                    ),
                   ),
-                )
-              ],
+                  Container(
+                    child: InkWell(
+                      onTap: () {},
+                      child: Text(
+                        "Forgot password?",
+                        style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            color: Colors.blue),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          )
-        ],
-      )))),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    child: ElevatedButton(
+                        onPressed: () {
+                          String pusername = username.text;
+                          String puserpassword = password.text;
+
+                          setState(() {
+                            if (pusername.isEmpty) {
+                              setState(() {
+                                usernamestatus = true;
+                              });
+                            }
+                            if (puserpassword.isEmpty) {
+                              setState(() {
+                                passwordstatus = true;
+                              });
+                            }
+                            // else {
+                            //   setState(() {
+                            //     usernamestatus = false;
+                            //     passwordstatus = false;
+                            //   });
+                            // }
+                          });
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text("LogIn Successsfully !"),
+                            duration: Duration(seconds: 2),
+                          ));
+                        },
+                        style: ElevatedButton.styleFrom(
+                            elevation: 10,
+                            shadowColor: Colors.black,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10))),
+                        child: Text("Sign in")),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.pushReplacement(context, MaterialPageRoute(
+                        builder: (context) {
+                          return signuppg();
+                        },
+                      ));
+                    },
+                    child: Container(
+                      child: RichText(
+                          text: TextSpan(children: [
+                        TextSpan(
+                            text: "Don't have account ? ",
+                            style:
+                                TextStyle(fontSize: 10, color: Colors.black)),
+                        TextSpan(
+                            text: "Sign Up",
+                            style: TextStyle(fontSize: 20, color: Colors.blue))
+                      ])),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        )))),
+      ),
     );
   }
 }
